b9efe2cab822b596671fc73dc9a697d3a7f2ab93
Ola-Domain <olasupooladotun6@gmail.com>
2023-01-10 21:25:40 +0000

    101

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -20,39 +20,28 @@ def print_stats(size, status_codes):
         print("{}: {}".format(key, status_codes[key]))
 
 if __name__ == "__main__":
-    import sys
+   import sys
 
-    size = 0
-    status_codes = {}
-    valid_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
-    count = 0
+status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+total_size = 0
 
-    try:
-        for line in sys.stdin:
-            if count == 10:
-                print_stats(size, status_codes)
-                count = 1
-            else:
-                count += 1
-
-            line = line.split()
-
-            try:
-                size += int(line[-1])
-            except (IndexError, ValueError):
-                pass
-
-            try:
-                if line[-2] in valid_codes:
-                    if status_codes.get(line[-2], -1) == -1:
-                        status_codes[line[-2]] = 1
-                    else:
-                        status_codes[line[-2]] += 1
-            except IndexError:
-                pass
-
-        print_stats(size, status_codes)
-
-    except KeyboardInterrupt:
-        print_stats(size, status_codes)
-        raise
+try:
+    line_count = 0
+    for line in sys.stdin:
+        line_count += 1
+        _, _, _, status_code, file_size = line.strip().split(" ")
+        status_code = int(status_code)
+        file_size = int(file_size)
+        status_codes[status_code] += 1
+        total_size += file_size
+        if line_count % 10 == 0:
+            print("Total file size: ", total_size)
+            for status_code in sorted(status_codes):
+                if status_codes[status_code] != 0:
+                    print(f"{status_code}: {status_codes[status_code]}")
+except KeyboardInterrupt:
+    print("Statistics since the beginning:")
+    print("Total file size: ", total_size)
+    for status_code in sorted(status_codes):
+        if status_codes[status_code] != 0:
+            print(f"{status_code}: {status_codes[status_code]}")
